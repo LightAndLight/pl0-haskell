d635ad330fa46e9e04b1effafc4ef1c48cbf8374
Isaac Elliott <isaace71295@gmail.com>
2016-06-01 22:17:58 +1000

    Use lens combinators in tests

--- test/Tests/PL0/StackMachine.hs
+++ test/Tests/PL0/StackMachine.hs
@@ -7,6 +7,7 @@ module Tests.PL0.StackMachine (stackMachineTests) where
 import           PL0.StackMachine
 import           PL0.StackMachine.Instruction
 
+import           Control.Lens
 import           Control.Monad.Except
 import           Control.Monad.IO.Class
 import           Control.Monad.State
@@ -40,9 +41,6 @@ instance Arbitrary NotIn where
     to <- suchThat arbitrary (< n)
     return $ NotIn n from to
 
-program :: [Instruction] -> Program
-program is = listArray (0,length is-1) is
-
 runTestStack :: TestStack a -> IO (Either MachineError a,MachineState)
 runTestStack test = do
   st <- newStack 10
@@ -50,43 +48,43 @@ runTestStack test = do
 
 prop_push :: Int -> Property
 prop_push n = monadicIO $ do
-  (_,MachineState _ _ (Stack sp vect)) <- run . runTestStack $ push n
+  (_,mstate) <- run . runTestStack $ push n
+  let sp = mstate ^. stackPointer
   assert $ sp == 0
-  res <- liftIO $ V.read vect sp
+  res <- liftIO $ V.read (mstate ^. stackData) sp
   assert $ res == n
 
 prop_peek :: Int -> Property
 prop_peek n = monadicIO $ do
-  (res,MachineState _ _ (Stack sp _)) <- run . runTestStack $ do
+  (res,mstate) <- run . runTestStack $ do
     push n
     peek
   assert $ res == Right n
-  assert $ sp == 0
+  assert $ mstate ^. stackPointer == 0
 
 prop_pop :: Int -> Property
 prop_pop n = monadicIO $ do
-  (res,MachineState _ _ (Stack sp _)) <- run . runTestStack $ do
+  (res,mstate) <- run . runTestStack $ do
     push n
     pop
   assert $ res == Right n
-  assert $ sp == -1
+  assert $ mstate ^. stackPointer == -1
 
 prop_negative_pop_error :: Negative -> Property
-prop_negative_pop_error (Negative sp') = monadicIO $ do
+prop_negative_pop_error (Negative sp) = monadicIO $ do
   (res,_) <- run . runTestStack $ do
-    (MachineState fp pc (Stack sp vect)) <- get
-    put $ MachineState fp pc (Stack sp' vect)
+    stackPointer .= sp
     pop
   assert $ res == Left StackUnderflow
 
 prop_div_by_zero_error :: Int -> Property
 prop_div_by_zero_error n = monadicIO $ do
-  res <- run . runProgramFrom 0 10 $ program [LOAD_CON n, ZERO, DIV, STOP]
+  res <- run . runProgramFrom 0 10 $ [LOAD_CON n, ZERO, DIV, STOP]
   assert $ res == Left DivByZero
 
 prop_range_error :: NotIn -> Property
 prop_range_error (NotIn n from to) = monadicIO $ do
-  res <- run . runProgramFrom 0 10 $ program [LOAD_CON n, LOAD_CON from, LOAD_CON to, BOUND, STOP]
+  res <- run . runProgramFrom 0 10 $ [LOAD_CON n, LOAD_CON from, LOAD_CON to, BOUND, STOP]
   assert $ res == Left (ValueOutOfRange n from to)
 
 return []
