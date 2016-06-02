604a4a16095b6699be5eb98bd744fc093924e7d4
Isaac Elliott <isaace71295@gmail.com>
2016-06-02 21:35:34 +1000

    Added HasScope class to MonadCode

--- src/PL0/CodeGen/Class.hs
+++ src/PL0/CodeGen/Class.hs
@@ -7,16 +7,23 @@ import           PL0.AST
 import           PL0.AST.Class
 import           PL0.SymbolTable.Scope
 
+import           Control.Lens
 import           Control.Monad.Except
 import           Control.Monad.State
 
-type MonadCode m = (MonadError String m, MonadState Scope m)
+class HasScope s where
+  scope :: Lens' s Scope
+
+instance HasScope Scope where
+  scope = id
+
+type MonadCode s m = (HasScope s, MonadError String m, MonadState s m)
 
 class Monoid c => Code c where
-  generate :: MonadCode m => Tree TypedExp -> m c
-  genBlock :: MonadCode m => Block TypedExp -> m c
-  genStatement :: MonadCode m => Statement TypedExp -> m c
-  genDeclarations :: MonadCode m => [Declaration TypedExp] -> m c
-  genExpression :: MonadCode m => TypedExp -> m c
+  generate :: MonadCode s m => Tree TypedExp -> m c
+  genBlock :: MonadCode s m => Block TypedExp -> m c
+  genStatement :: MonadCode s m => Statement TypedExp -> m c
+  genDeclarations :: MonadCode s m => [Declaration TypedExp] -> m c
+  genExpression :: MonadCode s m => TypedExp -> m c
   genOp :: OperationName -> c
   genAlloc :: Int -> c
