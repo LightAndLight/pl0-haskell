84335764949f31254ac95b4884fea605111e3668
Isaac Elliott <isaace71295@gmail.com>
2016-06-04 21:49:26 +1000

    Cleaned up Main

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,7 @@ import           PL0.StackMachine
 import           PL0.StaticChecker
 import           PL0.SymbolTable.Scope
 
+import           Control.Lens
 import           Control.Monad
 import           Control.Monad.Except
 import           Control.Monad.State
@@ -19,12 +20,10 @@ main = do
     [] -> putStrLn "Enter a file"
     (x:xs) -> do
       content <- readFile x
-      let tree = scanTokens content >>= parseTokens
-      case tree of
-        Right tree' -> do
-          let mprogram = checkProgram tree' >>= generate
-              program = flip evalState topLevelScope . runExceptT $ mprogram
-          case program of
-            Right program' -> runProgramFrom 0 1000 (instructions program') >>= print
-            Left err -> print err
+      let full = ExceptT . return . scanTokens
+             >=> ExceptT . return . parseTokens
+             >=> checkProgram
+             >=> generate
+      case flip evalState initialState . runExceptT . full $ content of
+        Right program' -> runProgramFrom 0 1000 (program' ^. instructions) >>= print
         Left err -> print err
