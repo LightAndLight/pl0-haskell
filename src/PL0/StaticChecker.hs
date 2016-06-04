596cedec855b642d8a3c9cde2f0dc34c0e708229
Isaac Elliott <isaace71295@gmail.com>
2016-06-04 21:56:24 +1000

    Use scope lens

--- src/PL0/StaticChecker.hs
+++ src/PL0/StaticChecker.hs
@@ -9,6 +9,7 @@ import           PL0.SymbolTable
 import           PL0.SymbolTable.Scope
 
 import           Control.Applicative   ((<|>))
+import Control.Lens
 import           Control.Monad
 import           Control.Monad.Except
 import           Control.Monad.State
@@ -16,58 +17,58 @@ import           Data.DList            (DList, singleton)
 import           Data.ITree.Zipper
 import qualified Data.Map              as M
 
-type MonadChecker m = (MonadError String m, MonadState Scope m)
+type MonadChecker s m = (HasScope s, MonadError String m, MonadState s m)
 
-checkProgram :: (Expression e, MonadChecker m) => Tree e -> m (Tree TypedExp)
+checkProgram :: (Expression e, MonadChecker s m) => Tree e -> m (Tree TypedExp)
 checkProgram (Tree block) = Tree <$> checkBlock block
 
-checkBlock :: (Expression e, MonadChecker m) => Block e -> m (Block TypedExp)
+checkBlock :: (Expression e, MonadChecker s m) => Block e -> m (Block TypedExp)
 checkBlock (Block decs statement) = Block
   <$> traverse checkDeclaration decs
   <*> checkStatement statement
 
-checkDeclaration :: (Expression e, MonadChecker m) => Declaration e -> m (Declaration TypedExp)
+checkDeclaration :: (Expression e, MonadChecker s m) => Declaration e -> m (Declaration TypedExp)
 checkDeclaration c@(ConstDef name expr)= do
-  (_,table) <- extractValue <$> get
+  table <- getTable <$> use scope
   if M.member name table
     then throwError $ name ++ " already defined"
     else do
       checked <- checkExpression expr
-      modify $ addEntry name (ConstEntry (getType checked) (Left checked))
+      scope %= addEntry name (ConstEntry (getType checked) (Left checked))
       return $ ConstDef name checked
 
 checkDeclaration t@(TypeDef name ty) = do
-  (_,table) <- extractValue <$> get
+  table <- getTable <$> use scope
   if M.member name table
     then throwError $ name ++ " already defined"
     else do
       resolvedTy <- resolve ty
-      modify $ addEntry name (TypeEntry resolvedTy)
+      scope %= addEntry name (TypeEntry resolvedTy)
       return $ TypeDef name resolvedTy
 
 checkDeclaration v@(VarDecl name ty) = do
-  (_,table) <- extractValue <$> get
+  table <- getTable <$> use scope
   if M.member name table
     then throwError $ name ++ " already declared"
     else do
       resolvedTy <- resolve ty
-      modify $ addEntry name (VarEntry (TRef resolvedTy) Nothing)
+      scope %= addEntry name (VarEntry (TRef resolvedTy) Nothing)
       return $ VarDecl name resolvedTy
 
 checkDeclaration p@(ProcedureDef name args block) = do
-  (_,table) <- extractValue <$> get
+  table <- getTable <$> use scope
   if M.member name table
     then throwError $ "procedure " ++ name ++ " already defined"
     else do
-      modify $ extendScope name
+      scope %= extendScope name
       checkedArgs <- traverse checkDeclaration args
       checkedBlock <- checkBlock block
-      modify up
+      scope %= leaveScope
       argTypes <- traverse (\(VarDecl _ ty) -> resolve ty) checkedArgs
-      modify $ addEntry name (ProcEntry argTypes Nothing)
+      scope %= addEntry name (ProcEntry argTypes Nothing)
       return $ ProcedureDef name checkedArgs checkedBlock
 
-checkStatement :: (Expression e, MonadChecker m) => Statement e -> m (Statement TypedExp)
+checkStatement :: (Expression e, MonadChecker s m) => Statement e -> m (Statement TypedExp)
 checkStatement (Assignment lvalue expr) = do
   checkedLValue <- checkExpression lvalue
   case checkedLValue of
@@ -97,7 +98,7 @@ checkStatement (If cond thenStatement elseStatement) = do
   else' <- checkStatement elseStatement
   return $ If cond' then' else'
 checkStatement c@(CallStatement name args) = do
-  scope <- get
+  scope <- use scope
   case findEntry name scope of
     Just (ProcEntry ty offset) -> do
       args' <- checkArgs ty =<< traverse checkExpression args
